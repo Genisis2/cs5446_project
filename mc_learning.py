f5de513f9ace1a9958441caa13b51cbf4d3bfc13
dingfan <zhaodingfanhaha@gmail.com>
2022-10-27 01:18:28 +0800

    feat: add basic LinearRegression template

--- mc_learning.py
+++ mc_learning.py
@@ -1,16 +1,35 @@
 import numpy as np
 import torch
-from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, LogisticRegression
+import pickle
 
 from data import train_dataset, test_dataset
 
+filename = 'finalized_model.sav'
+
+def _flatten(array):
+    return [item for sublist in array for item in sublist]
 
 def train() -> None:
-    print(len(train_dataset), len(test_dataset))
+    states, actions, rewards = train_dataset['states'], train_dataset['actions'], train_dataset['rewards']
+    
+    # flatten the game index - rallies from different games merged into one list
+    states, actions, rewards = _flatten(states), _flatten(actions), _flatten(rewards)
+    
+    model = LinearRegression()
+    model.fit(zip(states, actions), rewards)
+    print(model.score(zip(states, actions), rewards))
+
+    # store the model
+    pickle.dump(model, open(filename, 'wb'))
 
 
 def test() -> None:
-    pass
+    states, actions, rewards = test_dataset['states'], test_dataset['actions'], test_dataset['rewards']
+    states, actions, rewards = _flatten(states), _flatten(actions), _flatten(rewards)
+    loaded_model = pickle.load(open(filename, 'rb'))
+    result = loaded_model.score(zip(states, actions), rewards)
+    print(result)
 
 
 if __name__ == '__main__':
