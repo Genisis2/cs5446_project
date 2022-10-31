26efe73b2f45e5877b093907a8e41f8c9c1071d2
Hans Golong <hans.golong@gmail.com>
2022-10-31 16:16:58 +0800

    Including col information in the data extracted

--- data/__init__.py
+++ data/__init__.py
@@ -2,6 +2,6 @@
 Handles data retrieval
 """
 
-from .extractor import train_dataset, test_dataset
+from .extractor import train_dataset, test_dataset, hit_state_cols, hit_action_cols, opp_state_cols, opp_action_cols
 
-__all__ = [train_dataset, test_dataset]
+__all__ = [train_dataset, test_dataset, hit_state_cols, hit_action_cols, opp_state_cols, opp_action_cols]
